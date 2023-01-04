2f23cb6b47c0e203fa92c7cf51355450fceb66d7
Richard Baker <richard.baker@expensys.com>
2023-01-04 00:05:19 +0000

    two dice rolling sixes

--- src/app/game/dice/dice.page.ts
+++ src/app/game/dice/dice.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dice',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['dice.page.scss'],
 })
 
-export class DicePage {}
+export class DicePage {
+
+  constructor(private elem: ElementRef){}
+
+  public rollDice() {
+    //const dice = [...document.querySelectorAll(".die-list")];
+    const dice = this.elem.nativeElement.querySelectorAll('.die-list')
+    dice.forEach(die => {
+      this.toggleClasses(die);
+      die.dataset.roll = 6 //this.getRandomNumber(1, 6);
+    });
+  }
+  
+  private toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+  }
+  
+  private getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+}
 
